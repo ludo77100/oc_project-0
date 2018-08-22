a9f7a6a54e7722e3c1dda3368e4c447839c6c152
ludo77100 <ludovic.s@protonmail.com>
2018-08-22 15:17:28 +0200

    Add 2 commentary to method generateProductData in Class ProductRepository

--- src/main/java/com/lambazon/repository/ProductRepository.java
+++ src/main/java/com/lambazon/repository/ProductRepository.java
@@ -22,8 +22,8 @@ public class ProductRepository {
 		products.put(++id, new Product(id, 30, 69.99, "JVC HAFX8R Headphone", "Riptidz, In-Ear"));
 		products.put(++id, new Product(id, 40, 32.50, "VTech CS6114 DECT 6.0", "Cordless Phone"));
 		products.put(++id, new Product(id, 50, 895.00, "NOKIA OEM BL-5J", "Cell Phone "));
-		products.put(++id, new Product(id, 60, 138.99, "Nintendo 3DS XL", "Console"));
-		products.put(++id, new Product(id, 70, 900.00, "Acer E325-85", "Laptop"));
+		products.put(++id, new Product(id, 60, 138.99, "Nintendo 3DS XL", "Console")); //product add for system test
+		products.put(++id, new Product(id, 70, 900.00, "Acer E325-85", "Laptop")); //product add for system test
 	}
 	
 	public List<Product> products() {
